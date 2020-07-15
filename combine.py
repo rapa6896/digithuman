d74a7c03eb0ada4737c3fd0d310e657efac024c2
avish112 <avichaishiponi@gmail.com>
2020-07-16 01:10:33 +0300

    comb img by dec

--- combine.py
+++ combine.py
@@ -6,11 +6,10 @@ import os
 
 
 def combineImage(artist, genre, all, years, name_to_save):
-    geners = ['rock', 'pop', 'Classical', 'Jazz', 'Latin Music', 'Metal']
+    geners = ['Rock', 'Pop', 'Classical', 'Jazz', 'Latin Music', 'Metal']
     k = open('ndata.txt', 'r')
     info = json.load(k)
     imgs = []
-    c = 0
     for data in info['data']:
         if (artist != "" and artist == data["artist"]) or (genre != "" and data["main_category"] == genre) or (
                 all and data["main_category"] != "" and data["main_category"] in str(geners)):
@@ -25,9 +24,6 @@ def combineImage(artist, genre, all, years, name_to_save):
                     except:
                         continue
                     imgs.append(curImg)
-                    if (c == 24):
-                        print(data["image_path"])
-                    c += 1
 
     totalImgNum = len(imgs)
     imgnum = 0
@@ -41,7 +37,6 @@ def combineImage(artist, genre, all, years, name_to_save):
     except OSError:
         print ("Creation of the directory %s failed" % path)
         dir = ""
-
     for j in range(y):
         to_comb_row = []
         for i in range(x):
@@ -57,30 +52,39 @@ def combineImage(artist, genre, all, years, name_to_save):
     finle_img = combineImageCol(imgs)
     finle_img.show()
     finle_img.save(name_to_save + ".jpg")
-    
+
     return finle_img
 
 
 def combineImageRow(imgs):
     # pick the image which is the smallest, and resize the others to match it (can be arbitrary image shape here)
     min_shape = sorted([(np.sum(i.size), i.size) for i in imgs])[0][1]
-    imgs_comb = np.hstack((np.asarray(i.resize(min_shape)) for i in imgs))
+    h=[]
+    g=0
+    for i in imgs:
+        d=np.asarray(i.resize(min_shape))
+        try:
+            lenz=len(d[0][0])
+            succ=g
+        except:
+            d=np.asarray(imgs[succ].resize(min_shape))
+        h.append(d)
+        g+=1
+    imgs_comb = np.hstack(h)
 
     # save that beautiful picture
     imgs_comb = Image.fromarray(imgs_comb)
-    imgs_comb.save('jjjjjj.jpg')
     return imgs_comb
 
 
 def combineImageCol(imgs):
     # pick the image which is the smallest, and resize the others to match it (can be arbitrary image shape here)
     min_shape = sorted([(np.sum(i.size), i.size) for i in imgs])[0][1]
-    imgs_comb = np.hstack((np.asarray(i.resize(min_shape)) for i in imgs))
-
     # for a vertical stacking it is simple: use vstack
     imgs_comb = np.vstack((np.asarray(i.resize(min_shape)) for i in imgs))
     imgs_comb = Image.fromarray(imgs_comb)
     return imgs_comb
 
 
-combineImage("", "Jazz", False, [1960, 2016], "Jazz")
+combineImage("", "", True, [1960, 2016], "ALL")
+# ['Rock', 'Pop', 'Classical', 'Jazz', 'Latin Music', 'Metal']
--- ploting.py
+++ ploting.py
@@ -4,7 +4,6 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def j():
     print("l")
 
