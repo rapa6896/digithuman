8dae2d8c06d55db3f1ea244b8d0557b01259049e
avish112 <avichaishiponi@gmail.com>
2020-07-15 14:41:49 +0300

    init 1

--- ploting.py
+++ ploting.py
@@ -3,6 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def j():
+    print("l")
+
+
 def getMinIdx(myArray, image, N):
     min = image[myArray[0]][0]
     minIdx = 0
@@ -11,31 +15,34 @@ def getMinIdx(myArray, image, N):
             minIdx = i
     return minIdx
 
-def sameColor(a,b):
-    return (a[1]==b[1] and a[2]==b[2] and a[3]==b[3])
+
+def sameColor(a, b):
+    return (a[1] == b[1] and a[2] == b[2] and a[3] == b[3])
+
 
 def reduceImagePalette(imageColors):
-    temp=np.zeros([len(imageColors),4])
-    j=0
+    temp = np.zeros([len(imageColors), 4])
+    j = 0
     for i in imageColors:
-        temp[j]=[i[0],int(i[1][0]/10)*10,int(i[1][1]/10)*10,int(i[1][2]/10)*10]
-        j+=1
-    AfterReduce=[]
+        temp[j] = [i[0], int(i[1][0] / 10) * 10, int(i[1][1] / 10) * 10, int(i[1][2] / 10) * 10]
+        j += 1
+    AfterReduce = []
     for i in range(len(temp)):
-        if(temp[i][0]!=0):
-            for j in range(i+1,len(temp)):
-                if(sameColor(temp[i],temp[j])):
-                    temp[i][0]+=temp[j][0]
-                    temp[j][0]=0
+        if (temp[i][0] != 0):
+            for j in range(i + 1, len(temp)):
+                if (sameColor(temp[i], temp[j])):
+                    temp[i][0] += temp[j][0]
+                    temp[j][0] = 0
             AfterReduce.append(temp[i])
     print(len(imageColors))
     print(len(AfterReduce))
     return AfterReduce
 
+
 myImage = Image.open("D:/pics_for_digi/211.jpg")
-w,l=myImage.size
-myImageColors=myImage.getcolors(w*l)
-reducedImagePalette=reduceImagePalette(myImageColors)
+w, l = myImage.size
+myImageColors = myImage.getcolors(w * l)
+reducedImagePalette = reduceImagePalette(myImageColors)
 # print(myImageColors)
 # print(reducedImagePalette)
 # reducedImagePalette = myImage.convert('P', palette=Image.ADAPTIVE, colors=256)
@@ -54,9 +61,9 @@ for i in reducedImagePalette:
         dominantColors[minidx] = j
         minidx = getMinIdx(dominantColors, reducedImagePalette, N)
     j += 1
-theta = np.arange(0, 3.14 * 2, (2*3.14) /N, dtype=float)
+theta = np.arange(0, 3.14 * 2, (2 * 3.14) / N, dtype=float)
 radii = np.zeros(N)
-width = (2*3.14) / N
+width = (2 * 3.14) / N
 colors = np.zeros([N, 3])
 
 for i in range(N):
@@ -66,7 +73,6 @@ for i in range(N):
     radii[i] = reducedImagePalette[dominantColors[i]][0]
 print(colors)
 print(radii)
-
 
 ax = plt.subplot(111, projection='polar')
 ax.bar(theta, radii, width=width, bottom=0.0, color=colors, alpha=1)
